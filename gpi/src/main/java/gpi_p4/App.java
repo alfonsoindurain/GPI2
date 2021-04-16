2eb02ec23bb087b8091c33d51ce5ce4adefe6079
Alfonso <alfonsoindurain98@gmail.com>
2021-04-16 09:05:47 +0000

    Entrega 3 y 4

---
+++ gpi/src/main/java/gpi_p4/App.java
@@ -0,0 +1,17 @@
+package gpi_p4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getHelloWorld()  );
+    }
+
+    public static String getHelloWorld(){
+	    return "Hello World";
+    }		
+}
