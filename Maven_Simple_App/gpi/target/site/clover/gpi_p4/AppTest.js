ef15f952d46f228d50fa7471c4d2afac48a21330
Alfonso <alfonsoindurain98@gmail.com>
2021-04-27 19:04:04 +0000

    Practica 5 Maven

---
+++ Maven_Simple_App/gpi/target/site/clover/gpi_p4/AppTest.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":40,"id":4,"methods":[{"el":21,"sc":5,"sl":18},{"el":29,"sc":5,"sl":26},{"el":39,"sc":5,"sl":36}],"name":"AppTest","sl":10}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_0":{"methods":[{"sl":36}],"name":"testHelloWorld","pass":true,"statements":[{"sl":38}]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [0], [], [0], [], []]
---
+++ Maven_Simple_App/gpi/target/site/clover/package-nodes-tree.js
@@ -0,0 +1,33 @@
+ 
+
+var Packages = {
+    nodes: [
+                                                                                                        
+                
+{
+    "id": "gpi_p4",
+    "text": "gpi_p4",
+    "package": "gpi_p4",
+    "url": "gpi_p4/pkg-summary.html",
+            "coverage": "80%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "gpi_p4/pkg-summary.html"},
+    "children": [
+                    ]
+},
+            ],
+    settings: {
+        "icons": {
+            "package": {
+                "open": "aui-icon aui-icon-small aui-iconfont-devtools-folder-open",
+                "closed": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed"
+            },
+            "state": {
+                "collapsed": "aui-icon aui-icon-small aui-iconfont-collapsed",
+                "expanded": "aui-icon aui-icon-small aui-iconfont-expanded",
+                "forRemoval": "hidden aui-iconfont-collapsed aui-iconfont-expanded"
+            }
+        }
+    }
+};
